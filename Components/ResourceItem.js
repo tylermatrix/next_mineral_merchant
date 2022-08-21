6ad3784b40605e08ffd3633ab4037a8eff6ca74e
tylermatrix <tylermaatrix@gmail.com>
2022-08-21 00:57:14 -0400

    set layout for resource monitor

---
+++ Components/ResourceItem.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { Flex, Text } from "@chakra-ui/react";
+
+import { motion } from "framer-motion";
+function ResourceItem({ resource }) {
+  return (
+    <>
+      <Flex
+        width="90%"
+        height="65px"
+        background="#37364C"
+        boxShadow="4px 4px 18px #0A091A, -3px -3px 9px #646282"
+        borderRadius="20px"
+        justifyContent="space-evenly"
+        alignItems="center"
+        marginTop="25px"
+        alignSelf="center"
+        as={motion.div}
+        whileHover={{ scale: 0.99 }}
+        cursor="pointer"
+      >
+        <Text
+          fontSize="25px"
+          fontWeight="600"
+          lineHeight="37.5px"
+          width="165px"
+          height="38px"
+          color="#C1C1C1"
+          alignSelf="center"
+          as={motion.div}
+          whileHover={{ color: "white" }}
+        >
+          {resource}
+        </Text>
+
+        <Text
+          width="131px"
+          height="46px"
+          background="#53526B"
+          boxShadow="-3px -3px 9px #646282, inset 4px 4px 18px #0A091A"
+          borderRadius="20px"
+          alignSelf="center"
+        ></Text>
+      </Flex>
+    </>
+  );
+}
+
+export default ResourceItem;
--- Components/ResourceMonitor.js
+++ Components/ResourceMonitor.js
@@ -1,12 +1,34 @@
-import {Flex, Text} from '@chakra-ui/react'
+import { Flex, Text } from "@chakra-ui/react";
+import { motion } from "framer-motion";
+import ResourceItem from "./ResourceItem";
 export default function ResourceMonitor() {
   return (
-    <Flex
-      minW="387px"
-      height="481px"
-      direction="column"
-      bg="#353448"
-      borderRadius="15px"
-    ></Flex>
-  )
+    <>
+      <Flex
+        minW="387px"
+        height="481px"
+        direction="column"
+        bg="#353448"
+        borderRadius="15px"
+      >
+        <Flex>
+          <Text
+            fontFamily="Poppins"
+            fontStyle="normal"
+            fontWeight="600"
+            fontSize="25px"
+            lineHeight="38px"
+            color="#C1C1C1"
+            margin="25px"
+          >
+            Resource Monitor
+          </Text>
+        </Flex>
+        <ResourceItem resource="PLANET"></ResourceItem>
+        <ResourceItem resource="CASH"></ResourceItem>
+        <ResourceItem resource="DEBT"></ResourceItem>
+        <ResourceItem resource="DAYS"></ResourceItem>
+      </Flex>
+    </>
+  );
 }
